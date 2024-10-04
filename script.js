6f629abef51a51cf71d76293c9a4136b47fdfc18
Levuisha <157381495+Levuisha@users.noreply.github.com>
2024-10-04 18:33:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const pi = 3.14;
+function obim(Poluprecnik){
+    return 2*poluprecnik*pi;
+}
+
+
+
+const pravougaonik = function(a,b,p){
+    p=a*b;
+    return p;
+}
+console.log(pravougaonik(2,7));
+
+
+
